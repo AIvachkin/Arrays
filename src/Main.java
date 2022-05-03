f655de96c0e4182d695e6d94d7efb0e6dc6d21ee
Андрей <andrews.ru83@gmail.com>
2022-05-03 18:22:18 +0300

    Задача1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int[] arr = generateRandomArray();
-                  }
+        int expenses30days = 0 ;
+        for (int j : arr) {
+            //System.out.println(arr[i]);
+            expenses30days = expenses30days + j;
+        }
+        System.out.println("Сумма трат за месяц составила " + expenses30days + " рублей");
+        }
+
 
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
